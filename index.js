c69d3812ea69a54d0c0b3ea785ec378256ac15d5
KaziShoaib <shoaib.iiuc.cse@gmail.com>
2020-12-19 16:28:28 +0600

    added validation, changed frontend for handling validation, added production build of the front end

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ const { prependOnceListener } = require('./models/person')
 
 
 app.get('/api/persons', (request, response)=>{
-  Person.find({}).then(persons => {
-    response.json(persons);
-  })
+  Person.find({})
+    .then(returnedPersons => returnedPersons.map(person => person.toJSON()))
+    .then(returnedAndFormattedPersons => response.json(returnedAndFormattedPersons))
 })
 
 
@@ -53,7 +53,7 @@ app.get('/info', (request, response)=>{
 app.get('/api/persons/:id', (request, response, next)=>{
   Person.findById(request.params.id).then(person=>{
     if(person){
-      response.json(person);
+      response.json(person.toJSON());
     }
     else{
       response.status(404).end();
@@ -65,14 +65,12 @@ app.get('/api/persons/:id', (request, response, next)=>{
 
 app.delete('/api/persons/:id',(request, response, next)=>{
   Person.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end();
-    })
+    .then(result => response.status(204).end())
     .catch(error => next(error))
 })
 
 
-app.post('/api/persons', (request, response) =>{
+app.post('/api/persons', (request, response, next) =>{
   let body = request.body;
 
   if(body.name===undefined || body.number===undefined){
@@ -86,9 +84,10 @@ app.post('/api/persons', (request, response) =>{
     number: body.number,
   })
   
-  person.save().then(savedPerson=>{
-    response.json(savedPerson);
-  })
+  person.save()
+    .then(savedPerson=>savedPerson.toJSON())
+    .then(savedAndFormattedPerson=>response.json(savedAndFormattedPerson))
+    .catch(error => next(error))
 })
 
 
@@ -101,9 +100,14 @@ app.put('/api/persons/:id',(request, response, next)=>{
     number: body.number,
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, {new:true})
+  Person.findByIdAndUpdate(request.params.id, person, {new:true, runValidators: true, context: 'query'})
     .then(updatedPerson => {
-      response.json(updatedPerson);
+      if(updatedPerson){
+        response.json(updatedPerson.toJSON());
+      }
+      else{
+        response.status(404).end();
+      }
     })
     .catch(error => next(error))
 })
@@ -121,6 +125,9 @@ const errorHandler = (error, request, response, next) => {
   console.log(error.message);
   if(error.name === 'CastError'){
     return response.status(400).send({error:'malformatted id'});
+  }
+  else if(error.name === 'ValidationError'){
+    return response.status(400).json({error:error.message})
   }
   next(error);
 }
