2a5123b34f7d31fcebd2289c61a4dca036db3cda
KaziShoaib <shoaib.iiuc.cse@gmail.com>
2020-12-19 22:46:44 +0600

    implemented eslint

--- index.js
+++ index.js
@@ -1,140 +1,136 @@
-const { response, json } = require('express')
-const express = require('express') // express
-const app = express()
-app.use(express.json()) // parsing json from request body
+const express = require('express'); // express
+const app = express();
+app.use(express.json()); // parsing json from request body
 
 
 
-const cors = require('cors')  // connecting front end with backend
-app.use(cors())
+const cors = require('cors');  // connecting front end with backend
+app.use(cors());
 
-app.use(express.static('build')) // connecting front end production build
+app.use(express.static('build')); // connecting front end production build
 
-require('dotenv').config()  // accessing the environment variables from the .env file
+require('dotenv').config();  // accessing the environment variables from the .env file
 
 
 //morgan middlewear
-const morgan = require('morgan') 
-morgan.token('post_data', (req, res)=>{
-  if(req.method === 'POST'){
-    return JSON.stringify(req.body);
-  }
-  else{
-    return '';
-  }
-})
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post_data'))
+const morgan = require('morgan');
+morgan.token('post_data', (req, res) => {
+	if(req.method === 'POST'){
+		return JSON.stringify(req.body);
+	}
+	else{
+		return '';
+	}
+});
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post_data'));
 
 
-const Person = require('./models/person')
-const { prependOnceListener } = require('./models/person')
+const Person = require('./models/person');
 
 
-app.get('/api/persons', (request, response)=>{
-  Person.find({})
-    .then(returnedPersons => returnedPersons.map(person => person.toJSON()))
-    .then(returnedAndFormattedPersons => response.json(returnedAndFormattedPersons))
-})
+app.get('/api/persons', (request, response) => {
+	Person.find({})
+		.then(returnedPersons => returnedPersons.map(person => person.toJSON()))
+		.then(returnedAndFormattedPersons => response.json(returnedAndFormattedPersons));
+});
 
 
 //info about how many names are there in the database
-app.get('/info', (request, response)=>{
-  
-  Person.find({}).then(persons=>{
-    const length = persons.length;
-    let p1 = `<p>Phonebook has info for ${length} people</p>`;
-    let p2 = `<p>${Date()}</p>`
-    response.send(p1+p2);
-  })
-  
-})
+app.get('/info', (request, response) => {
+
+	Person.find({}).then(persons => {
+		const length = persons.length;
+		let p1 = `<p>Phonebook has info for ${length} people</p>`;
+		let p2 = `<p>${Date()}</p>`;
+		response.send(p1+p2);
+	});
+});
 
 
-app.get('/api/persons/:id', (request, response, next)=>{
-  Person.findById(request.params.id).then(person=>{
-    if(person){
-      response.json(person.toJSON());
-    }
-    else{
-      response.status(404).end();
-    }
-  })
-  .catch(error=>next(error)) // sending error to the errorHandler middleware
-})
+app.get('/api/persons/:id', (request, response, next ) => {
+	Person.findById(request.params.id).then(person => {
+		if(person){
+			response.json(person.toJSON());
+		}
+		else{
+			response.status(404).end();
+		}
+	})
+		.catch(error => next(error)); // sending error to the errorHandler middleware
+});
 
 
-app.delete('/api/persons/:id',(request, response, next)=>{
-  Person.findByIdAndRemove(request.params.id)
-    .then(result => response.status(204).end())
-    .catch(error => next(error))
-})
+app.delete('/api/persons/:id',(request, response, next) => {
+	Person.findByIdAndRemove(request.params.id)
+		.then(result => response.status(204).end())
+		.catch(error => next(error));
+});
 
 
-app.post('/api/persons', (request, response, next) =>{
-  let body = request.body;
+app.post('/api/persons', (request, response, next) => {
+	let body = request.body;
 
-  if(body.name===undefined || body.number===undefined){
-    return response.status(400).json({
-      'error':'name or number missing'
-    });
-  }
+	if(body.name===undefined || body.number===undefined){
+		return response.status(400).json({
+			'error':'name or number missing'
+		});
+	}
 
-  let person = new Person({
-    name: body.name,
-    number: body.number,
-  })
-  
-  person.save()
-    .then(savedPerson=>savedPerson.toJSON())
-    .then(savedAndFormattedPerson=>response.json(savedAndFormattedPerson))
-    .catch(error => next(error))
-})
+	let person = new Person({
+		name: body.name,
+		number: body.number,
+	});
+	person.save()
+		.then(savedPerson => savedPerson.toJSON())
+		.then(savedAndFormattedPerson => response.json(savedAndFormattedPerson))
+		.catch(error => next(error));
+});
 
 
-app.put('/api/persons/:id',(request, response, next)=>{
-  const body = request.body;
+app.put('/api/persons/:id',(request, response, next) => {
+	const body = request.body;
 
-  //creating a normal javascript object instaead of a mongoose model
-  let person = {
-    name: body.name,
-    number: body.number,
-  }
+	//creating a normal javascript object instaead of a mongoose model
+	let person = {
+		name: body.name,
+		number: body.number,
+	};
 
-  Person.findByIdAndUpdate(request.params.id, person, {new:true, runValidators: true, context: 'query'})
-    .then(updatedPerson => {
-      if(updatedPerson){
-        response.json(updatedPerson.toJSON());
-      }
-      else{
-        response.status(404).end();
-      }
-    })
-    .catch(error => next(error))
-})
+	Person.findByIdAndUpdate(request.params.id, person, { new:true, runValidators: true, context: 'query' })
+		.then(updatedPerson => {
+			if(updatedPerson){
+				response.json(updatedPerson.toJSON());
+			}
+			else{
+				response.status(404).end();
+			}
+		})
+		.catch(error => next(error));
+});
 
 
 // unknown endpoint middleware
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({error : 'unknown endpoint'})
-}
+	response.status(404).send({ error : 'unknown endpoint' });
+};
 app.use(unknownEndpoint);
 
 
 // error handler middleware
 const errorHandler = (error, request, response, next) => {
-  console.log(error.message);
-  if(error.name === 'CastError'){
-    return response.status(400).send({error:'malformatted id'});
-  }
-  else if(error.name === 'ValidationError'){
-    return response.status(400).json({error:error.message})
-  }
-  next(error);
-}
+	console.log(error.message);
+	if(error.name === 'CastError'){
+		return response.status(400).send({ error:'malformatted id' });
+	}
+	else if(error.name === 'ValidationError'){
+		return response.status(400).json({ error:error.message });
+	}
+	next(error);
+};
 app.use(errorHandler);
 
 
-const PORT = process.env.PORT
-app.listen(PORT, ()=>{
-  console.log(`listening to port ${PORT}`);
-})
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+	console.log(`listening to port ${PORT}`);
+});
--- mongo.js
+++ mongo.js
@@ -1,40 +1,40 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 if(process.argv.length !== 3 && process.argv.length !== 5){
-  console.log('usage :\nnode mongo.js <passwod>\nnode mongo.js <password> <name> <phone-number>\n');
-  process.exit(1);
+	console.log('usage :\nnode mongo.js <passwod>\nnode mongo.js <password> <name> <phone-number>\n');
+	process.exit(1);
 }
 
 
 const password = process.argv[2];
 const dbName = 'phonebook-app';
-const url = `mongodb+srv://shoaib:${password}@cluster0.rf6od.mongodb.net/${dbName}?retryWrites=true&w=majority`
+const url = `mongodb+srv://shoaib:${password}@cluster0.rf6od.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-})
+	name: String,
+	number: String,
+});
 
 const Person = mongoose.model('Person', personSchema);
 
 if(process.argv.length === 3){
-  Person.find({}).then(result=>{
-    console.log("phonebook:");
-    result.forEach(person => {
-      console.log(`${person.name} ${person.number}`);
-    });
-    mongoose.connection.close();
-  })
+	Person.find({}).then(result => {
+		console.log('phonebook:');
+		result.forEach(person => {
+			console.log(`${person.name} ${person.number}`);
+		});
+		mongoose.connection.close();
+	});
 }
 else{
-  let person = new Person({
-    name: process.argv[3],
-    number: process.argv[4],
-  });
-  person.save().then(result=>{
-    console.log(`added ${person.name} number ${person.number} to phonebook`);
-    mongoose.connection.close();
-  })
+	let person = new Person({
+		name: process.argv[3],
+		number: process.argv[4],
+	});
+	person.save().then(result => {
+		console.log(`added ${person.name} number ${person.number} to phonebook`);
+		mongoose.connection.close();
+	});
 }
